edc8cf54dcc934ec56bef214351200bafbfbac06
manojkumar747 <138973139+manojkumar747@users.noreply.github.com>
2023-07-13 10:22:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var on_bin=document.getElementById("on");
+var on_bin=document.getElementById("off");
+var img=document.getElementById("bulb");
+
+function bulb_on(){
+    img.src="./download1.jpg"
+}
+
+function bulb_off(){
+    img.src="./download 2.jpg"
+}
